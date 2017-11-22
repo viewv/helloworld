cda89c24f4b52548f38c3f752b8f46cffb4e28f8
Zhang XueNan <zxn@zxnnet.top>
2017-11-22 21:36:14 +0800

    First ed. about 11 programming languages

---
+++ helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+namespace helloworld
+{
+    class helloworld
+    {
+        static void mMin(string[] args)
+        {
+            Console.WriteLines("helloworld");
+            Console.ReadKey();
+        }
+    }
+}
